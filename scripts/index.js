b580a8ba0699000de7d1b536b73fcb1349956f78
Andrey275 <brus4a1ka@yandex.ru>
2025-03-27 19:53:51 +0300

    created functional create and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = link;
+  cardImage.alt = name;
 
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardTitle.textContent = name;
+
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', function () {
+    deleteCard(cardElement)
+  });
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+  card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+  placesList.append(createCard(card.name, card.link, deleteCard));
+});
